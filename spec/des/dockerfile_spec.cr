dd9081270dcff60b801c63fb1cb192d560868c97
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-06-16 18:37:23 +0900

    fixing dockerfile spec

--- spec/des/dockerfile_spec.cr
+++ spec/des/dockerfile_spec.cr
@@ -1,7 +1,7 @@
 require "../spec_helper"
 
 describe Des::Dockerfile do
-  describe "#setup_image" do
+  describe "#validate_image!" do
     [
       {"input" => "ubuntu"},
       {"input" => "ubuntu:16.04"},
@@ -9,11 +9,9 @@ describe Des::Dockerfile do
       {"input" => "crystallang/crystal"},
       {"input" => "crystallang/crystal:latest"},
     ].each do |spec_case|
-      it "returns image name when valid pattern. spec pattern -> #{spec_case["input"]}" do
-        opts = Clim::Options::Values.new
-        opts.string = {"image" => spec_case["input"]}
-        dockerfile = Dockerfile.new(Rc.new, "", opts)
-        dockerfile.setup_image.should eq spec_case["input"]
+      it "not raises an Exception when valid pattern. spec pattern -> #{spec_case["input"]}" do
+        dockerfile = Dockerfile.new(Rc.new, "", Clim::Options::Values.new)
+        dockerfile.validate_image!(spec_case["input"]).should be_nil # not raises an Exception
       end
     end
     [
@@ -26,36 +24,51 @@ describe Des::Dockerfile do
       {"input" => "aaa/bbb:ccc:ddd"},
     ].each do |spec_case|
       it "raises an Exception when invarid image name. spec pattern -> #{spec_case["input"]}" do
-        opts = Clim::Options::Values.new
-        opts.string = {"image" => spec_case["input"]}
-        dockerfile = Dockerfile.new(Rc.new, "", opts)
+        dockerfile = Dockerfile.new(Rc.new, "", Clim::Options::Values.new)
         expect_raises(Exception, "Invalid image name pattern. Valid pattern -> /\\A[a-zA-Z][^:]+?(|:[0-9a-zA-Z-\._]+?)\\z/") do
-          dockerfile.setup_image
+          dockerfile.validate_image!(spec_case["input"])
         end
       end
     end
   end
-  describe "#setup_packages" do
-    [
-      {"input" => [] of String},
-      {"input" => ["curl", "vim", "apache-2utils"]},
-    ].each do |spec_case|
-      it "returns packages when valid package names. spec pattern -> #{spec_case["input"]}" do
-        opts = Clim::Options::Values.new
-        opts.array = {"packages" => spec_case["input"]}
-        dockerfile = Dockerfile.new(Rc.new, "", opts)
-        dockerfile.setup_packages.should eq spec_case["input"]
-      end
+  describe "#update_to_config_param" do
+    it "non update image and packages when 'default_param' key does not exists in conf file." do
+      opts = Clim::Options::Values.new
+      opts.string = {"image" => "default_image"}
+      opts.array = {"packages" => ["default_package1", "default_package1"]}
+      rc = Rc.new(File.expand_path("./spec/des/dockerfile/.desrc_not_default_param_key.yml"))
+      dockerfile = Dockerfile.new(rc, "", opts)
+      dockerfile.update_to_config_param
+      dockerfile.image.should eq "ubuntu:latest" # non update
+      dockerfile.packages.should eq [] of String # non update
     end
-  end
-  describe "#setup_config_param" do
+    it "non update image when 'default_image' key does not exists." do
+      opts = Clim::Options::Values.new
+      opts.string = {"image" => "default_image"}
+      opts.array = {"packages" => ["default_package1", "default_package1"]}
+      rc = Rc.new(File.expand_path("./spec/des/dockerfile/.desrc_not_image_key.yml"))
+      dockerfile = Dockerfile.new(rc, "", opts)
+      dockerfile.update_to_config_param
+      dockerfile.image.should eq "ubuntu:latest" # non update
+      dockerfile.packages.should eq ["desrc_package1", "desrc_package2"] # update
+    end
+    it "non update packages when 'default_packages' key does not exists." do
+      opts = Clim::Options::Values.new
+      opts.string = {"image" => "default_image"}
+      opts.array = {"packages" => ["default_package1", "default_package1"]}
+      rc = Rc.new(File.expand_path("./spec/des/dockerfile/.desrc_not_packages_key.yml"))
+      dockerfile = Dockerfile.new(rc, "", opts)
+      dockerfile.update_to_config_param
+      dockerfile.image.should eq "desrc_image" # update
+      dockerfile.packages.should eq [] of String # non update
+    end
     it "update image and packages when .desrc.yml exists." do
       opts = Clim::Options::Values.new
       opts.string = {"image" => "default_image"}
       opts.array = {"packages" => ["default_package1", "default_package1"]}
       rc = Rc.new(File.expand_path("./spec/des/dockerfile/.desrc.yml"))
       dockerfile = Dockerfile.new(rc, "", opts)
-      dockerfile.setup_config_param
+      dockerfile.update_to_config_param
       dockerfile.image.should eq "desrc_image"
       dockerfile.packages.should eq ["desrc_package1", "desrc_package2"]
     end
--- src/des/dockerfile.cr
+++ src/des/dockerfile.cr
@@ -13,32 +13,49 @@ module Des
     def initialize(@rc, @container_name, @opts)
     end
 
-    def setup_image
-      image = @opts.s["image"]
+    def update_to_config_param
+      @image = _find_image_in_conf
+      @packages = _find_packages_in_conf
+    end
+
+    private def _find_image_in_conf
+      return @image unless @rc.setting["default_param"]?
+      return @image unless @rc.setting["default_param"]["image"]?
+      @rc.setting["default_param"]["image"].as_s
+    end
+
+    private def _find_packages_in_conf
+      return @packages unless @rc.setting["default_param"]?
+      return @packages unless @rc.setting["default_param"]["packages"]?
+      @rc.setting["default_param"]["packages"].map(&.as_s)
+    end
+
+    def update_to_command_param
+      @image = _find_image_in_opts
+      @packages = _find_packages_in_opts
+    end
+
+    private def _find_image_in_opts
+      return @image unless @opts.s["image"]?
+      validate_image!(@opts.s["image"])
+      @opts.s["image"]
+    end
+
+    def validate_image!(image)
       pattern = "\\A[a-zA-Z][^:]+?(|:[0-9a-zA-Z-\._]+?)\\z"
       unless image.match /#{pattern}/
         raise "Invalid image name pattern. Valid pattern -> /#{pattern}/"
       end
-      image
     end
 
-    def setup_packages
+    private def _find_packages_in_opts
+      return @packages unless @opts.s["packages"]?
       @opts.a["packages"]
     end
 
-    def setup_config_param
-      @image = @rc.setting["default_param"]["image"]?.to_s || @image
-      @packages = @rc.setting["default_param"]["packages"].to_a || @packages
-    end
-
-    def setup_command_param
-      @image = setup_image
-      @packages = setup_packages
-    end
-
     def create_file
-      setup_config_param
-      setup_command_param
+      update_to_config_param
+      update_to_command_param
     end
   end
 end
