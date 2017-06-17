2a75cf61f4b12bede2f8a0119ab6afbcdaf4f761
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-06-18 06:38:28 +0900

    cli option indent

--- src/cli.cr
+++ src/cli.cr
@@ -5,13 +5,13 @@ module Des
     main_command
     desc "Creates docker environment skeleton."
     usage "des [options]"
-    string "-i IMAGE", "--image=IMAGE", desc: "Base docker image name.", default: "ubuntu:16.04"
-    array "-p PACKAGES", "--packages=PACKAGE", desc: "apt-get install packages name.", default: [] of String, required: false
-    string "-c NAME", "--container=NAME", desc: "Container name.", default: "my_container"
-    string "-d SAVE_DIR", "--save-dir=SAVE_DIR", desc: "Save dir path.", default: "."
-    string "-r RC_FILE", "--rc-file=RC_FILE", desc: ".descr.yml path.", default: "~/.desrc.yml"
-    string "--mysql=VERSION", desc: "Add mysql container with version specified.", default: ""
-    string "--nginx=VERSION", desc: "Add nginx container with version specified.", default: ""
+    string "-i IMAGE", "--image=IMAGE",          desc: "Base docker image name.",                     default: "ubuntu:16.04"
+    array  "-p PACKAGES", "--packages=PACKAGE",  desc: "apt-get install packages name.",              default: [] of String
+    string "-c NAME", "--container=NAME",        desc: "Container name.",                             default: "my_container"
+    string "-d SAVE_DIR", "--save-dir=SAVE_DIR", desc: "Save dir path.",                              default: "."
+    string "-r RC_FILE", "--rc-file=RC_FILE",    desc: ".descr.yml path.",                            default: "~/.desrc.yml"
+    string "--mysql-version=VERSION",            desc: "Add mysql container with version specified.", default: ""
+    string "--nginx-version=VERSION",            desc: "Add nginx container with version specified.", default: ""
     run do |opts, args|
 
       opts = Opts.new(opts)
