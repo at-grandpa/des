2051e54310bba654da54bea30ec8809f74d5055f
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-06-18 00:08:53 +0900

    fix build error

--- src/cli.cr
+++ src/cli.cr
@@ -1,25 +1,27 @@
 require "./des"
 
-class Cli < Clim
-  main_command
-  desc "Creates docker environment skeleton."
-  usage "des [options]"
-  string "-i IMAGE", "--image=IMAGE", desc: "Base docker image name.", default: "ubuntu:16.04"
-  array "-p PACKAGES", "--packages=PACKAGE", desc: "apt-get install packages name.", default: [] of String, required: false
-  string "-c CONTAINER_NAME", "--container-name=CONTAINER_NAME", desc: "Container name.", default: "my_container"
-  string "-d SAVE_DIR", "--save-dir=SAVE_DIR", desc: "Save dir path.", default: "."
-  string "-r RC_FILE", "--rc-file=RC_FILE", desc: ".descr.yml path.", default: "~/.desrc.yml"
-  string "--mysql=VERSION", desc: "Add mysql container with version specified.", default: ""
-  string "--nginx=VERSION", desc: "Add nginx container with version specified.", default: ""
-  run do |opts, args|
-    opts = Opts.new(opts)
-    rc = Rc.new(File.read(opts.rc_file))
+module Des
+  class Cli < Clim
+    main_command
+    desc "Creates docker environment skeleton."
+    usage "des [options]"
+    string "-i IMAGE", "--image=IMAGE", desc: "Base docker image name.", default: "ubuntu:16.04"
+    array "-p PACKAGES", "--packages=PACKAGE", desc: "apt-get install packages name.", default: [] of String, required: false
+    string "-c CONTAINER_NAME", "--container-name=CONTAINER_NAME", desc: "Container name.", default: "my_container"
+    string "-d SAVE_DIR", "--save-dir=SAVE_DIR", desc: "Save dir path.", default: "."
+    string "-r RC_FILE", "--rc-file=RC_FILE", desc: ".descr.yml path.", default: "~/.desrc.yml"
+    string "--mysql=VERSION", desc: "Add mysql container with version specified.", default: ""
+    string "--nginx=VERSION", desc: "Add nginx container with version specified.", default: ""
+    run do |opts, args|
+      opts = Opts.new(opts)
+      rc = Rc.new(File.read(opts.rc_file))
 
-    dockerfile_parameters = Dockerfile::Parameters.new(rc, opts)
-    Dockerfile::FileCreator.new(dockerfile_parameters).create_file
-    # DockerCompose.new(opts).create_file
-    # Makefile.new(opts).create_file
+      dockerfile_params = Dockerfile::Parameters.new(rc, opts)
+      Dockerfile::FileCreator.new(dockerfile_params).create_file
+      # DockerCompose.new(opts).create_file
+      # Makefile.new(opts).create_file
+    end
   end
 end
 
-Cli.start(ARGV)
+Des::Cli.start(ARGV)
--- src/des/dockerfile/file_creator.cr
+++ src/des/dockerfile/file_creator.cr
@@ -10,7 +10,7 @@ module Des
         File.write("#{@parameters.save_dir}/#{FILE_NAME}", to_s)
       end
 
-      ECR.def_to_s "#{__DIR__}/dockerfile/template.ecr"
+      ECR.def_to_s "#{__DIR__}/template.ecr"
     end
   end
 end
