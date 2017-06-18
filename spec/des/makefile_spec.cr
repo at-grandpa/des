41e09f15a0312d2a25ad11a2c25a2a52e019ecd0
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-06-19 05:36:12 +0900

    add Makefile spec

---
+++ spec/des/makefile_spec.cr
@@ -0,0 +1,126 @@
+require "../spec_helper"
+
+alias OptsParameter = Hash(String, String) | Hash(String, Bool) | Hash(String, Array(String))
+
+class Makefile::SpecCase
+  @describe : String = ""
+  @rc_file_yaml : String = ""
+  @opts_parameters : Array(OptsParameter) = [] of OptsParameter
+  @expect : String = ""
+
+  getter describe, rc_file_yaml, opts_parameters, expect
+
+  def initialize(@describe, @rc_file_yaml, opts_parameters, @expect)
+    opts_parameters.each do |parameter|
+      @opts_parameters << parameter
+    end
+  end
+end
+
+describe Des::Makefile do
+  describe "#create_file" do
+    [
+      Makefile::SpecCase.new(
+        describe: "create Makefile with rc_file parameter when there is no opts parameters.",
+        rc_file_yaml: "
+        default_param:
+          image: rc_file_image
+          packages:
+            - rc_file_package1
+            - rc_file_package2
+          container: rc_file_container
+          save_dir: #{__DIR__}/makefile/rc_file_save_dir
+          web_app: false
+        ",
+        opts_parameters: [] of OptsParameter,
+        expect: <<-EXPECT
+        DOCKER_COMPOSE := docker-compose -f ./docker-compose.yml
+        DOCKER_EXEC := docker exec -it
+        CONTAINER_NAME := rc_file_container
+
+        ps:
+        	$(DOCKER_COMPOSE) ps
+
+        build:
+        	$(DOCKER_COMPOSE) build
+
+        up:
+        	$(DOCKER_COMPOSE) up -d
+
+        clean: stop rm
+
+        stop:
+        	$(DOCKER_COMPOSE) stop
+
+        rm:
+        	$(DOCKER_COMPOSE) rm -f
+
+        attach:
+        	$(DOCKER_EXEC) $(CONTAINER_NAME) /bin/bash
+
+
+        EXPECT
+      ),
+      Makefile::SpecCase.new(
+        describe: "create Makefile overwrited 'container' with the opts parameter when opts has an 'container'.",
+        rc_file_yaml: "
+        default_param:
+          image: rc_file_image
+          packages:
+            - rc_file_package1
+            - rc_file_package2
+          container: rc_file_container
+          save_dir: #{__DIR__}/makefile/rc_file_save_dir
+          web_app: false
+        ",
+        opts_parameters: [
+          {"container" => "opts_container"},
+        ],
+        expect: <<-EXPECT
+        DOCKER_COMPOSE := docker-compose -f ./docker-compose.yml
+        DOCKER_EXEC := docker exec -it
+        CONTAINER_NAME := opts_container
+
+        ps:
+        	$(DOCKER_COMPOSE) ps
+
+        build:
+        	$(DOCKER_COMPOSE) build
+
+        up:
+        	$(DOCKER_COMPOSE) up -d
+
+        clean: stop rm
+
+        stop:
+        	$(DOCKER_COMPOSE) stop
+
+        rm:
+        	$(DOCKER_COMPOSE) rm -f
+
+        attach:
+        	$(DOCKER_EXEC) $(CONTAINER_NAME) /bin/bash
+
+
+        EXPECT
+      ),
+    ].each do |spec_case|
+      it spec_case.describe do
+        rc = Rc.new(spec_case.rc_file_yaml)
+
+        input_opts = Clim::Options::Values.new
+        spec_case.opts_parameters.each do |parameter|
+          input_opts.merge!(parameter)
+        end
+        opts = Opts.new(input_opts)
+
+        parameters = Parameters.new(rc, opts)
+        Makefile.new(parameters, silent: true).create_file
+
+        created_file_path = "#{rc.save_dir}/Makefile"
+        File.read(created_file_path).should eq spec_case.expect
+        File.delete(created_file_path)
+      end
+    end
+  end
+end
