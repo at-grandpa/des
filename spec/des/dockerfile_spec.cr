9da0e382c602b1d3c345156b740a113ca4ae2029
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-06-19 04:24:38 +0900

    add dockerfile spec

--- spec/des/dockerfile_spec.cr
+++ spec/des/dockerfile_spec.cr
@@ -1,38 +1,63 @@
 require "../spec_helper"
 
+alias OptsParameters = Hash(String, String) | Hash(String, Bool) | Hash(String, Array(String))
+
+class SpecCase
+  @describe : String = ""
+  @rc_file_yaml : String = ""
+  @opts_parameters : Array(OptsParameters) = [] of OptsParameters
+  @expect : String = ""
+
+  getter describe, rc_file_yaml, opts_parameters, expect
+
+  def initialize(@describe, @rc_file_yaml, @opts_parameters, @expect)
+  end
+end
+
 describe Des::Dockerfile do
   describe "#create_file" do
-    it "create file with rc_file parameter when there is no opts parameters." do
-      yaml_str = <<-YAML
-      default_param:
-        image: rc_file_image
-        packages:
-          - rc_file_package1
-          - rc_file_package2
-        container: rc_file_container
-        save_dir: #{__DIR__}/dockerfile/rc_file_save_dir
-        web_app: false
-      YAML
-      rc = Rc.new(yaml_str)
+    [
+      SpecCase.new(
+        describe: "create file with rc_file parameter when there is no opts parameters.",
+        rc_file_yaml: <<-RC_FILE_YAML
+        default_param:
+          image: rc_file_image
+          packages:
+            - rc_file_package1
+            - rc_file_package2
+          container: rc_file_container
+          save_dir: #{__DIR__}/dockerfile/rc_file_save_dir
+          web_app: false
+        RC_FILE_YAML,
+        opts_parameters: [] of OptsParameters,
+        expect: <<-EXPECT
+        FROM rc_file_image
 
-      input_opts = Clim::Options::Values.new
-      opts = Opts.new(input_opts) # There is no parameters of option.
+        RUN apt-get -y update
+        RUN apt-get -y upgrade
+        RUN apt-get -y install rc_file_package1 rc_file_package2
 
-      parameters = Parameters.new(rc, opts)
-      Dockerfile.new(parameters, silent: true).create_file
+        WORKDIR /root/rc_file_container
 
-      created_file_path = "#{rc.save_dir}/Dockerfile"
-      File.read(created_file_path).should eq <<-CREATED_FILE
-      FROM rc_file_image
+        EXPECT
+      ),
+    ].each do |spec_case|
+      it spec_case.describe do
+        rc = Rc.new(spec_case.rc_file_yaml)
 
-      RUN apt-get -y update
-      RUN apt-get -y upgrade
-      RUN apt-get -y install rc_file_package1 rc_file_package2
+        input_opts = Clim::Options::Values.new
+        spec_case.opts_parameters.each do |parameter|
+          input_opts.merge!(parameter)
+        end
+        opts = Opts.new(input_opts)
 
-      WORKDIR /root/rc_file_container
+        parameters = Parameters.new(rc, opts)
+        Dockerfile.new(parameters, silent: true).create_file
 
-      CREATED_FILE
-      File.delete(created_file_path)
+        created_file_path = "#{rc.save_dir}/Dockerfile"
+        File.read(created_file_path).should eq spec_case.expect
+        File.delete(created_file_path)
+      end
     end
     it "create Dockerfile overwrited 'image' with the opts parameter when opts has an 'image'." do
       yaml_str = <<-YAML
