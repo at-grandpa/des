d8b881acab8e4f5404d0a5e837af2ec9bf75e9ac
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-06-19 05:11:54 +0900

    fix dockerfile multi spec case

--- spec/des/dockerfile_spec.cr
+++ spec/des/dockerfile_spec.cr
@@ -2,7 +2,7 @@ require "../spec_helper"
 
 alias OptsParameter = Hash(String, String) | Hash(String, Bool) | Hash(String, Array(String))
 
-class SpecCase
+class Dockerfile::SpecCase
   @describe : String = ""
   @rc_file_yaml : String = ""
   @opts_parameters : Array(OptsParameter) = [] of OptsParameter
@@ -10,14 +10,17 @@ class SpecCase
 
   getter describe, rc_file_yaml, opts_parameters, expect
 
-  def initialize(@describe, @rc_file_yaml, @opts_parameters, @expect)
+  def initialize(@describe, @rc_file_yaml, opts_parameters, @expect)
+    opts_parameters.each do |parameter|
+      @opts_parameters << parameter
+    end
   end
 end
 
 describe Des::Dockerfile do
   describe "#create_file" do
     [
-      SpecCase.new(
+      Dockerfile::SpecCase.new(
         describe: "create file with rc_file parameter when there is no opts parameters.",
         rc_file_yaml: "
         default_param:
@@ -32,6 +35,32 @@ describe Des::Dockerfile do
         opts_parameters: [] of OptsParameter,
         expect: <<-EXPECT
         FROM rc_file_image
+
+        RUN apt-get -y update
+        RUN apt-get -y upgrade
+        RUN apt-get -y install rc_file_package1 rc_file_package2
+
+        WORKDIR /root/rc_file_container
+
+        EXPECT
+      ),
+      Dockerfile::SpecCase.new(
+        describe: "create Dockerfile overwrited 'image' with the opts parameter when opts has an 'image'.",
+        rc_file_yaml: "
+        default_param:
+          image: rc_file_image
+          packages:
+            - rc_file_package1
+            - rc_file_package2
+          container: rc_file_container
+          save_dir: #{__DIR__}/dockerfile/rc_file_save_dir
+          web_app: false
+        ",
+        opts_parameters: [
+          {"image" => "opts_image"},
+        ],
+        expect: <<-EXPECT
+        FROM opts_image
 
         RUN apt-get -y update
         RUN apt-get -y upgrade
@@ -58,39 +87,6 @@ describe Des::Dockerfile do
         File.read(created_file_path).should eq spec_case.expect
         File.delete(created_file_path)
       end
-    end
-    it "create Dockerfile overwrited 'image' with the opts parameter when opts has an 'image'." do
-      yaml_str = <<-YAML
-      default_param:
-        image: rc_file_image
-        packages:
-          - rc_file_package1
-          - rc_file_package2
-        container: rc_file_container
-        save_dir: #{__DIR__}/dockerfile/rc_file_save_dir
-        web_app: false
-      YAML
-      rc = Rc.new(yaml_str)
-
-      input_opts = Clim::Options::Values.new
-      input_opts.merge!({"image" => "opts_image"})
-      opts = Opts.new(input_opts) # There is no parameters of option.
-
-      parameters = Parameters.new(rc, opts)
-      Dockerfile.new(parameters, silent: true).create_file
-
-      created_file_path = "#{rc.save_dir}/Dockerfile"
-      File.read(created_file_path).should eq <<-CREATED_FILE
-      FROM opts_image
-
-      RUN apt-get -y update
-      RUN apt-get -y upgrade
-      RUN apt-get -y install rc_file_package1 rc_file_package2
-
-      WORKDIR /root/rc_file_container
-
-      CREATED_FILE
-      File.delete(created_file_path)
     end
   end
 end
