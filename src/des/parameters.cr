35bbfeb0713da238e4a215263dd41349750efcf4
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-06-21 08:30:00 +0900

    Revert "..."
    This reverts commit ce0b7602a8e4e9dcca8d1a5fcc99c318e15d93f3.

--- src/des/parameters.cr
+++ src/des/parameters.cr
@@ -4,28 +4,29 @@ module Des
   class Parameters
     @opts : Des::Opts
     @rc : Des::Rc
-    @image : String | Nil
-    @packages : Array(String) | Nil
-    @container : String | Nil
-    @save_dir : String | Nil
-    @web_app : Bool | Nil
-    @overwrite : Bool | Nil
+    @image : String
+    @packages : Array(String)
+    @container : String
+    @save_dir : String
+    @web_app : Bool
+    @overwrite : Bool
 
     getter image, packages, container, save_dir, web_app, overwrite
 
     def initialize(@rc, @opts)
       @image = _find_image
-      # @packages = _find_packages
-      # @container = _find_container
-      # @save_dir = _find_save_dir
-      # @web_app = _find_web_app
-      # @overwrite = _find_overwrite
+      @packages = _find_packages
+      @container = _find_container
+      @save_dir = _find_save_dir
+      @web_app = _find_web_app
+      @overwrite = _find_overwrite
     end
 
     private def _find_image
       image = nil
       image = @rc.image unless @rc.image.nil?
-      # image = @opts.image unless @opts.image.nil?
+      image = @opts.image unless @opts.image.nil?
+      raise "Image name is not set. See 'des -h'" if image.nil?
       image
     end
 
@@ -33,6 +34,7 @@ module Des
       packages = nil
       packages = @rc.packages unless @rc.packages.nil?
       packages = @opts.packages unless @opts.packages.nil?
+      raise "Packages is not set. See 'des -h'" if packages.nil?
       packages
     end
 
@@ -40,6 +42,7 @@ module Des
       container = nil
       container = @rc.container unless @rc.container.nil?
       container = @opts.container unless @opts.container.nil?
+      raise "Container name is not set. See 'des -h'" if container.nil?
       container
     end
 
@@ -47,6 +50,7 @@ module Des
       save_dir = nil
       save_dir = @rc.save_dir unless @rc.save_dir.nil?
       save_dir = @opts.save_dir unless @opts.save_dir.nil?
+      raise "Save dir is not set. See 'des -h'" if save_dir.nil?
       save_dir
     end
 
@@ -54,6 +58,7 @@ module Des
       web_app = nil
       web_app = @rc.web_app unless @rc.web_app.nil?
       web_app = @opts.web_app unless @opts.web_app.nil?
+      raise "web_app flag is not set. See 'des -h'" if web_app.nil?
       web_app
     end
 
@@ -61,6 +66,7 @@ module Des
       overwrite = nil
       overwrite = @rc.overwrite unless @rc.overwrite.nil?
       overwrite = @opts.overwrite unless @opts.overwrite.nil?
+      raise "overwrite flag is not set. See 'des -h'" if overwrite.nil?
       overwrite
     end
   end
--- src/des/rc.cr
+++ src/des/rc.cr
@@ -2,10 +2,6 @@ require "yaml"
 
 module Des
   class Rc
-    def initialize
-      @setting = YAML.parse("")
-    end
-
     def initialize(yaml_str)
       @setting = YAML.parse(yaml_str)
       begin
@@ -16,54 +12,35 @@ module Des
     end
 
     def image
-      setting = @setting
-      return nil if setting.nil?
-      return nil unless setting.responds_to?(:[]?)
-      p typeof(setting.as_nil)
-      p typeof(nil)
-      p setting
-      p setting.nil?
-      return nil unless setting["default_param"]?
-      # return nil unless setting["default_param"]["image"]?
-      # setting["default_param"]["image"].as_s
+      return nil unless @setting["default_param"]?
+      return nil unless @setting["default_param"]["image"]?
+      @setting["default_param"]["image"].as_s
     end
 
     def packages
-      setting = @setting
-      return nil if setting.nil?
-      return nil unless setting.responds_to?(:[]?)
-      return nil unless setting["default_param"]?
-      return nil unless setting["default_param"]["packages"]?
-      setting["default_param"]["packages"].map(&.as_s)
+      return nil unless @setting["default_param"]?
+      return nil unless @setting["default_param"]["packages"]?
+      @setting["default_param"]["packages"].map(&.as_s)
     end
 
     def container
-      setting = @setting
-      return nil if setting.nil?
-      return nil unless setting.responds_to?(:[]?)
-      return nil unless setting["default_param"]?
-      return nil unless setting["default_param"]["container"]?
-      setting["default_param"]["container"].as_s
+      return nil unless @setting["default_param"]?
+      return nil unless @setting["default_param"]["container"]?
+      @setting["default_param"]["container"].as_s
     end
 
     def save_dir
-      setting = @setting
-      return nil if setting.nil?
-      return nil unless setting.responds_to?(:[]?)
-      return nil unless setting["default_param"]?
-      return nil unless setting["default_param"]["save_dir"]?
-      save_dir = setting["default_param"]["save_dir"].as_s
+      return nil unless @setting["default_param"]?
+      return nil unless @setting["default_param"]["save_dir"]?
+      save_dir = @setting["default_param"]["save_dir"].as_s
       raise "Save dir set as rc_file is not found. -> #{save_dir}" unless Dir.exists?(save_dir)
       save_dir
     end
 
     def web_app
-      setting = @setting
-      return nil if setting.nil?
-      return nil unless setting.responds_to?(:[]?)
-      return nil unless setting["default_param"]?
-      return nil unless setting["default_param"]["web_app"]?
-      web_app_str = setting["default_param"]["web_app"].as_s
+      return nil unless @setting["default_param"]?
+      return nil unless @setting["default_param"]["web_app"]?
+      web_app_str = @setting["default_param"]["web_app"].as_s
       case web_app_str
       when "true"
         true
@@ -75,12 +52,9 @@ module Des
     end
 
     def overwrite
-      setting = @setting
-      return nil if setting.nil?
-      return nil unless setting.responds_to?(:[]?)
-      return nil unless setting["default_param"]?
-      return nil unless setting["default_param"]["overwrite"]?
-      overwrite_str = setting["default_param"]["overwrite"].as_s
+      return nil unless @setting["default_param"]?
+      return nil unless @setting["default_param"]["overwrite"]?
+      overwrite_str = @setting["default_param"]["overwrite"].as_s
       case overwrite_str
       when "true"
         true
